eb381087af776755e5a0f590bf8e0befc948e473
l.baysaa <l.baysaa@9f459dbe-2a23-11de-bcab-dfcfbda92187>
2009-04-21 10:12:20 +0000

    nemelt

--- 1C/calendar.java
+++ 1C/calendar.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 public class calendar extends JFrame{
+	calendar() {
+		super("Calendar");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		add(panel);
+		pack();
+		setVisible(true);
+	}
 	public static void main(String args[])
 	{
-		
+		new calendar();
 	}
 }
