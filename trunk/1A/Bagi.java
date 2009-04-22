2e319e2f7312e139b7faba512a3f5b7696bb23e3
infinitybrain <infinitybrain@9f459dbe-2a23-11de-bcab-dfcfbda92187>
2009-04-22 10:33:23 +0000

    1A

---
+++ trunk/1A/Bagi.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+
+public class Notepad extends JFrame //implements ActionListener
+{
+	public static void main(String args[])
+	{
+		Notepad frame = new Notepad();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		frame.setTitle("Note");
+		frame.setSize(400,500);
+	}
+}
